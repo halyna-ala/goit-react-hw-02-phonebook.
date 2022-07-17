f421a122d526e0a07c51f1e8747e3c1bea1807c5
halyna-ala <dawihalyna@gmail.com>
2022-07-17 23:52:49 +0200

    phone

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -2,8 +2,13 @@ import { InputFilter, LabelFilter } from "./Filter.styled";
 import {PropTypes} from 'prop-types'
 
 const Filter = ({filter, onChange}) => (
-	<LabelFilter>
-		<InputFilter type="text" value={filter} onChange={onChange} /> Find contacts by name
+	<LabelFilter htmlFor="search"> Find contacts by name
+		<InputFilter  id ="search"
+		type="search"
+		value={filter} 
+		onChange={onChange} 
+		pattern=".*\S.*"
+        required/> 
 	</LabelFilter>
 )
 
